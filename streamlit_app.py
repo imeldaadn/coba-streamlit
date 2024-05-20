5dae3aeef3b3a5ee4da4346c099c74980bcc5ac6
imeldaaudina <126536048+imeldaadn@users.noreply.github.com>
2024-05-21 00:41:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,9 @@
-#######################
-# Import libraries
 import streamlit as st
 import mysql.connector
 import pandas as pd
 import altair as alt
 import plotly.express as px
 
-#######################
 # Page configuration
 st.set_page_config(
     page_title="Data Visualization Dashboard",
@@ -16,7 +13,6 @@ st.set_page_config(
 
 alt.themes.enable("dark")
 
-#######################
 # Load data
 mydb = mysql.connector.connect(
     host = "localhost",
@@ -25,25 +21,21 @@ mydb = mysql.connector.connect(
     username = "root",
     password = "",
 )
-#######################
+
 # Sidebar
 def main():
-st.sidebar:
-    st.title('🏂 Data Visualization Dashboard')
+    st.sidebar.title('🏂 Data Visualization Dashboard')
     option = st.sidebar.selectbox("Select a Database Option", ("Data Warehouse Adventureworks", "Web Scrapping"))
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 
-
-#######################
 # Data AW
 ## Dashboard
 st.header('Hi, Welcome to the :rainbow[***Data Visualization Dashboard!***]', divider='rainbow')
 st.markdown('We are here to showcase your data :orange[in] a cool :orange[and] engaging way :sunglasses:')
-
 st.subheader("Data Warehouse Adventureworks")
 
 ## Grafik
 
-if __name__ == "__streamlit_app__":
+if __name__ == "__main__":
     main()
